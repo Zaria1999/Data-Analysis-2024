a324097e43c1aaa99ca3df1026f273abe968c670
Zaria1999 <136509649+Zaria1999@users.noreply.github.com>
2024-09-23 11:44:04 -0400

    Add files via upload

---
+++ ANOVA-TEST-SUM.R
@@ -0,0 +1,7 @@
+ summary(anova_one_way)
+            Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+> 
